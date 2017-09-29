f29218ed1b556094e3433bdba0dfdf309f11e0fb
chenfangming <chenfangming@91zdan.com>
2017-09-29 18:07:58 +0800

    1

---
+++ core/src/main/java/me/geemu/persistence/dao/blog/BlogUserInfoMapper.java
@@ -0,0 +1,7 @@
+package me.geemu.persistence.dao.blog;
+
+import me.geemu.persistence.entity.blog.BlogUserInfo;
+import me.geemu.util.MyMapper;
+
+public interface BlogUserInfoMapper extends MyMapper<BlogUserInfo> {
+}
---
+++ core/src/main/java/me/geemu/persistence/dao/passtool/PasstoolUserInfoMapper.java
@@ -0,0 +1,7 @@
+package me.geemu.persistence.dao.passtool;
+
+import me.geemu.persistence.entity.passtool.PasstoolUserInfo;
+import me.geemu.util.MyMapper;
+
+public interface PasstoolUserInfoMapper extends MyMapper<PasstoolUserInfo> {
+}
