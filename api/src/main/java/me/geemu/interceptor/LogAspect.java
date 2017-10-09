6e79d365a6275672f8a1c4dec6ed6d62657a1d12
chenfangming <chenfangming@91zdan.com>
2017-10-09 17:06:53 +0800

    1

--- api/src/main/java/me/geemu/interceptor/LogAspect.java
+++ api/src/main/java/me/geemu/interceptor/LogAspect.java
@@ -51,7 +51,9 @@ public class LogAspect {
         BaseResponse<Object> response = new BaseResponse<Object>();
         StringBuilder logSb = new StringBuilder("\r\n");
         logSb.append(String.format("[TargetMethod]\t%s\r\n", targetMethod));
-        logSb.append(String.format("[RequestParams]\t\t%s\r\n", JSON.toJSONString(requestParams)));
+        logSb.append(String.format("[RequestParams]\t%s\r\n", JSON.toJSONString(requestParams)));
+        logSb.append(String.format("[ResponseBody]\t%s\r\n", JSON.toJSONString(response)));
+        logSb.append(String.format("[RequestURL]\t%s\r\n", attributes.getRequest().getRequestURL()));
         if (e instanceof BusinessException) {
             BusinessException be = (BusinessException) e;
             response.setCode(BaseResponseEnum.DEFAULT_BUSINESS_ERROR.getCode());
@@ -59,12 +61,10 @@ public class LogAspect {
             if (be.getCode() != null) {
                 response.setCode(be.getCode());
             }
-            logSb.append(String.format("[ResponseBody]\t%s\r\n", JSON.toJSONString(response)));
             logger.error(String.format("[发生了自定义异常]\t%s\r", logSb.toString()));
         } else {
             response.setCode(BaseResponseEnum.DEFAULT_UNKNOW_ERROR.getCode());
             response.setMessage(BaseResponseEnum.DEFAULT_UNKNOW_ERROR.getMessage());
-            logSb.append(String.format("[ResponseBody]\t%s\r\n", JSON.toJSONString(response)));
             logger.error(String.format("[后台未知异常，请联系开发小哥]\t%s\r", logSb.toString()));
         }
 
